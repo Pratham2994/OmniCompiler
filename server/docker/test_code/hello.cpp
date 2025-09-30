cc5b084beddaeff7c02164b1847a0f043b9aece3
CLONER786 <makwanakrishn2004@gmail.com>
2025-09-30 22:41:34 +0530

    created docker folder and dockerfiles for every language

---
+++ server/docker/test_code/hello.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    vector<int> a = {1, 2, 3};
+    cout<<"hi from cpp file"<<endl;
+    
+    int num;
+    cout<<"Enter a number"<<endl;
+    cin>>num;
+    for(auto i:a){
+        cout<<i<<endl;
+    }
+
+    cout<<"number u uentered "<<num;
+    return 0;
+}
---
+++ server/docker/test_code/hello.py
@@ -0,0 +1,25 @@
+from import_hello import SayHello
+
+# import numpy as np
+# import pandas as pd
+
+# data = np.random.randint(5, 35, size = 7)
+# df = pd.DataFrame(data, columns = ['integers'])
+
+# # displaying random integers in data frame
+# print("Before Sorting :")
+# print(df)
+
+# df.sort_values("integers", axis = 0, ascending = True,
+#                 inplace = True, na_position ='last')
+
+# print("After Sorting :")
+# print(df)
+
+SayHello()
+
+n = int(input("enter a number "))
+for i in range(n):
+    print("i =", i)
+
+print(input("Enter anything you wish "))
